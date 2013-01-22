0c6a3728bce552d91518976ca2eecfbb1ceca699
Krzysiek <krzysiek@UBUNTU-LAP.(none)>
2013-01-22 18:43:24 +0100

    fixed & updated

--- ubuntu-programs.sh
+++ ubuntu-programs.sh
@@ -6,31 +6,44 @@ fi
 mkdir temp
 cd temp
 
-# mate desktop environment (Ubu 12.10)
-sudo add-apt-repository "deb http://packages.mate-desktop.org/repo/ubuntu quantal main"
-sudo apt-get update
-sudo apt-get install mate-archive-keyring
-sudo apt-get update
-sudo apt-get install mate-core mate-desktop-environment
 
 #bumblebee
 sudo add-apt-repository ppa:bumblebee/stable
 sudo apt-get update
 sudo apt-get install bumblebee bumblebee-nvidia linux-headers-generic
 
-# /etc/fstab
-# /dev/sda6	/media/dokumenty	ntfs	defaults	0	0
+echo "/dev/sda6	/media/dokumenty	ntfs	defaults	0	0" >> /etc/fstab
+
+
+
+echo "/etc/apt/sources.list"
+echo "Uncomment two lines to add software from Canonical's"
+echo "'partner' repository."
+echo "tray: dconf: Com > Canonical > Desktop > Unity > Panel to ['all']"
+
+echo -n "press <enter> to continue"
+read continue
+
 
 # google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
 sudo dpkg -i google-chrome-stable_current_i386.deb
 
+# Ubuntu tweek
+sudo add-apt-repository ppa:tualatrix/ppa
+sudo apt-get update && sudo apt-get install ubuntu-tweak
+
 #indicators
 # indicator-keylock indicator-multiload
-sudo add-apt-repository ppa:tsbarnes/indicator-keylock-daily && sudo apt-get update
-sudo apt-get install indicator-keylock
+sudo add-apt-repository ppa:tsbarnes/indicator-keylock-daily
+sudo add-apt-repository ppa:indicator-multiload/stable-daily 
+sudo apt-get update
+sudo apt-get install indicator-multiload indicator-keylock
 
-sudo apt-get install gnote audacious mc cheese-common dixygen doxygen-latex git gnote gnuplot goldendict gparted guake inkscape k3b libk3b6-extracodecs sox manpages-pl manpages-pl-dev manpages-posix manpages-posix-dev mplayer okular openjdk-7-jre php5-cli php5-common rhino ruby pidgin samba skype virtualbox vlc wine exuberant-ctags
+
+sudo apt-get install gnote anki audacious audacity mc cheese-common doxygen doxygen-latex kile texlive-lang-polish git gnote gnuplot goldendict gparted guake inkscape k3b libk3b6-extracodecs sox manpages-pl manpages-pl-dev manpages-posix manpages-posix-dev mplayer okular openjdk-7-jre php5-cli php5-common rhino ruby pidgin samba skype virtualbox vlc wine exuberant-ctags account-plugin-gadugadu compizconfig-settings-manager gimp gimp-plugin-registry touchegg
+
+sudo apt-get remove rhythmbox
 
 # vim
 git clone https://github.com/palkakrzysiek/vim-startup.git
@@ -40,4 +53,5 @@ chmod +x ./vim-startup.sh && ./vim-startup.sh
 
 echo "http://ankisrs.net/anki2.html"
 echo "Zeitgeist"
+
 
